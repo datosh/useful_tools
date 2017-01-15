5a57eacf59f456ea0fcbe8a159e6d617dbaae3f4
Fabian Kammel <datosh@gmx.de>
2017-01-15 15:48:10 +0100

    Added basics for deploy project. Able to create CPP Project with testing and cmake

---
+++ deploy_project/README.md
@@ -0,0 +1,10 @@
+## Tool to deploy a new CPP Project.
+
+### Automates the annoying task to create a sekelton for each project
+
+#### TODO: 
+
+- Make install so this can be called from everywhere
+- Include clang-format & clang-tidy
+- Include doxygen
+- Include other languages?
---
+++ deploy_project/deploy.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import sys
+
+folders_to_create = ["src", "include", "build", "test"]
+
+# CMakeLists.txt for base directory
+# We need to fill:
+#   project_name
+#   project_exe
+#   project_test_exe
+#   project_test_name
+#
+base_cmake_path = "CMakeLists.txt"
+base_cmake = """
+cmake_minimum_required(VERSION 3.1.0 FATAL_ERROR)
+
+# force clang
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
+
+project(##PROJECTNAME##)
+
+# add compiler flags
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O2 -std=c++1z")
+
+# add includes
+include_directories(${PROJECT_SOURCE_DIR}/include)
+
+# Build daugther once so we can get all function needed in instrumentation
+add_executable(##PROJECTNAME##
+    ${PROJECT_SOURCE_DIR}/src/main.cpp)
+
+add_subdirectory(test)
+enable_testing()
+add_test(NAME ##PROJECTNAME##_test COMMAND ##PROJECTNAME##_test)
+"""
+
+# CMakeLists.txt for test directory
+# We need to fill:
+#   project_test_exe
+#   project_test_file
+#
+test_cmake_path = "test/CMakeLists.txt"
+test_cmake = """
+find_package(GTest REQUIRED)
+include_directories(${GTEST_INCLUDE_DIRS})
+
+add_executable(##PROJECTNAME##_test
+               ${CMAKE_CURRENT_LIST_DIR}/test.cpp)
+target_link_libraries(##PROJECTNAME##_test
+                      ${GTEST_BOTH_LIBRARIES}
+                      pthread)
+"""
+
+# src/main.cpp
+main_cpp_path = "src/main.cpp"
+main_cpp = """
+#include <iostream>
+
+int main(int argc, char **argv) {
+
+    std::cout << "Hello: ##PROJECTNAME##\\n";
+    return 0;
+}
+
+"""
+
+# test/test.cpp
+test_cpp_path = "test/test.cpp"
+test_cpp = """
+#include "gtest/gtest.h"
+
+TEST(##PROJECTNAME##Test, Positive) {
+    EXPECT_EQ(1, 1);
+}
+
+TEST(##PROJECTNAME##Test, Negative) {
+    EXPECT_NE(1, 2);
+}
+
+GTEST_API_ int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+"""
+
+files_to_create = [
+    [base_cmake, base_cmake_path],
+    [test_cmake, test_cmake_path],
+    [main_cpp, main_cpp_path],
+    [test_cpp, test_cpp_path],
+]
+
+
+def checkUsage():
+    if len(sys.argv) != 3:
+        print("Usage: " + sys.argv[0] + " <path> <projectName>")
+        sys.exit(-1)
+
+
+def create_folders(project_path):
+    for folder in folders_to_create:
+        folder_path = os.path.join(project_path, folder)
+        if not os.path.exists(folder_path):
+            os.mkdir(folder_path)
+
+
+def populate_names(project_name):
+    placeholder = "##PROJECTNAME##"
+
+    global files_to_create
+    for i in range(len(files_to_create)):
+        files_to_create[i][0] = files_to_create[i][0].replace(
+            placeholder, project_name)
+
+
+def create_files(project_path):
+    for content, path in files_to_create:
+        file_path = os.path.join(project_path, path)
+        with open(file_path, 'w') as f:
+            f.write(content)
+
+
+def main():
+    checkUsage()
+
+    # Get project name
+    project_name = sys.argv[2]
+
+    # Work on absolute path to prevent any confusion
+    path = os.path.abspath(sys.argv[1])
+    print("Going to deplay project: '" + project_name + "' in: " + path)
+
+    # Check and create root
+    project_path = os.path.join(path, project_name)
+    if not os.path.exists(project_path):
+        os.mkdir(project_path)
+
+    create_folders(project_path)
+    populate_names(project_name)
+    create_files(project_path)
+
+    subprocess.call(["tree", project_path])
+
+
+if __name__ == '__main__':
+    main()
